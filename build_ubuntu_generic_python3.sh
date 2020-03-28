0512a77141af50c3ec6246406ada1ac187a2b418
Richard Dally <r.dally@protonmail.com>
2020-03-28 12:39:42 +0100

    Fix bug in build_ubuntu_generic_python3.sh

--- build_ubuntu_generic_python3.sh
+++ build_ubuntu_generic_python3.sh
@@ -5,7 +5,7 @@ PYTHON_MINOR=7
 PYTHON_MICRO=6
 
 docker build -f dockerfile.ubuntu_generic_python3 \
-       -t richarddally/ubuntu_generic_python3:$PYTHON_MAJOR.$PYTHON_MICRO.$PYTHON_MICRO \
+       -t richarddally/ubuntu_generic_python3:$PYTHON_MAJOR.$PYTHON_MINOR.$PYTHON_MICRO \
        --build-arg PYTHON_MAJOR=$PYTHON_MAJOR \
        --build-arg PYTHON_MINOR=$PYTHON_MINOR \
        --build-arg PYTHON_MICRO=$PYTHON_MICRO \
